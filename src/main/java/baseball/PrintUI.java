77af31d63fe9b45c951db1df1f64aa80ab756c7d
soulfever <soulfever01@naver.com>
2021-12-15 15:26:28 +0900

    FEAT : 숫자를 입력받는 기능 추가

--- src/main/java/baseball/PrintUI.java
+++ src/main/java/baseball/PrintUI.java
@@ -1,2 +1,18 @@
-package baseball;public class PrintUI {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
+public class PrintUI {
+    public static ArrayList<Integer> InputThreeNum() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        String[] inputNumbers = input.split("");
+        ArrayList<Integer> threeNum = new ArrayList<>();
+        for (String i : inputNumbers) {
+            threeNum.add(Integer.parseInt(i));
+        }
+        return threeNum;
+    }
 }
